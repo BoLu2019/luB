d80d489397bacdc048a67fdc648db026c6eee77e
BoLu2019 <blu4@stuy.edu>
2018-10-04 22:26:16 -0400

    finish

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -14,16 +14,38 @@ c = db.cursor()               #facilitate db ops
 
 #==========================================================
 #INSERT YOUR POPULATE CODE IN THIS ZONE
-command = "CREATE TABLE students(name TEXT,age INTEGER,userid INTEGER)"
-c.execute(command)
+
+command = "CREATE TABLE students(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT,age INTEGER,userid INTEGER)"
+
+c.execute(command)#runs statement
+
+#populates students table
 with open('peeps.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         command="INSERT INTO students(name, age, userid) VALUES ('"
-        command+= row['name'] + "'," + row['age'] + "," + row['id']
+        command+= row['name'] + "', " + row['age'] + ", " + row['id']
         command+= ");" 
-        c.execute(command)   
+        c.execute(command)
 
+
+
+#time for courses.csv
+
+command = "CREATE TABLE courses(id INTEGER PRIMARY KEY AUTOINCREMENT, code TEXT, mark INTEGER, userid INTEGER)"
+
+c.execute(command)#runs statement
+
+#populates courses table
+with open('courses.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        command="INSERT INTO courses(code, mark, userid) VALUES ('"
+        command+= row['code'] + "', " + row['mark'] + ", " + row['id']
+        command+= ");" 
+        c.execute(command)
+
+        
 #==========================================================
 
 db.commit() #save changes
